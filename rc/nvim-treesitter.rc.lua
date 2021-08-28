5260db8604af00b683fed11dfee2b5a02159d4e9
uplus <uplus.e10@gmail.com>
2021-08-28 12:28:22 +0900

    treesitter-textobjects

--- rc/nvim-treesitter.rc.lua
+++ rc/nvim-treesitter.rc.lua
@@ -1,4 +1,3 @@
-
 require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
@@ -7,5 +6,55 @@ require'nvim-treesitter.configs'.setup {
   indent = {
     enable = true,
     disable = {'python', 'ruby'},
+  },
+  textobjects = {
+    enable = true
+  },
+  textobjects = {
+    select = {
+      enable = true,
+
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+
+        -- Or you can define your own textobjects like this
+        ["iF"] = {
+          python = "(function_definition) @function",
+          cpp = "(function_definition) @function",
+          c = "(function_definition) @function",
+          java = "(method_declaration) @function",
+        },
+      },
+    },
+    move = {
+      enable = true,
+
+      -- Whether to set jumps in the jumplist
+      set_jumps = true,
+
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+    },
   },
 }
