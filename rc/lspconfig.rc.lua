dbf30d7c618b8ffba0e5ab5c5377ac24043c1dc5
uplus <uplus.e10@gmail.com>
2022-04-20 11:44:46 +0900

    update

--- rc/lspconfig.rc.lua
+++ rc/lspconfig.rc.lua
@@ -32,9 +32,9 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', ',ai', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
   buf_set_keymap('n', ',as', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
   buf_set_keymap('n', ',at', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', ',aR', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', ',ar', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
 
-  buf_set_keymap('n', ',ar', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', ',aR', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   buf_set_keymap('n', ',aa', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   buf_set_keymap('n', ',af', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
