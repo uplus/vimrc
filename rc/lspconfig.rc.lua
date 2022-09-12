974feeff8fff8875abb1fe8e347c606440168ee1
uplus <uplus.e10@gmail.com>
2022-09-12 17:59:20 +0900

    update

--- rc/lspconfig.rc.lua
+++ rc/lspconfig.rc.lua
@@ -48,12 +48,18 @@ local flags = {
 -- :help lspconfig-server-configurations
 -- "marksman"
   -- h1が複数あると警告が出て邪魔
-for _, server in ipairs({ "pyright", "rust_analyzer", "tsserver", "gopls", "graphql", "dockerls", "metals" }) do
+for _, server in ipairs({ "pyright", "rust_analyzer", "tsserver", "gopls", "graphql", "dockerls", }) do
   nvim_lsp[server].setup {
     on_attach = on_attach,
     flags = flags,
   }
 end
+
+nvim_lsp["metals"].setup {
+  on_attach = on_attach,
+  flags = flags,
+  filetypes = { "scala", "sbt", "java" },
+}
 
 nvim_lsp["yamlls"].setup {
   on_attach = on_attach,
