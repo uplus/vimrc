f684f6738fc813798a144616a8ab0694707011f6
uplus <uplus.e10@gmail.com>
2023-09-30 15:28:39 +0900

    aerial

--- rc/aerial.rc.lua
+++ rc/aerial.rc.lua
@@ -8,18 +8,36 @@ require('aerial').setup({
   lsp = {
     -- Fetch document symbols when LSP diagnostics update.
     -- If false, will update on buffer changes.
-    diagnostics_trigger_update = false,
+    diagnostics_trigger_update = true,
+
+    -- Set to false to not update the symbols when there are LSP errors
+    update_when_errors = false,
   },
 
   layout = {
     default_direction = 'float',
 
-    -- open aerial at the far right/left of the editor
-    placement_editor_edge = true,
-
+    -- These control the width of the aerial window.
+    -- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+    -- min_width and max_width can be a list of mixed types.
+    -- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
     max_width = { 40, 0.3 },
     width = nil,
     min_width = 40,
+
+    -- key-value pairs of window-local options for aerial window (e.g. winhl)
+    win_opts = {},
+
+    -- Determines where the aerial window will be opened
+    --   edge   - open aerial at the far right/left of the editor
+    --   window - open aerial to the right/left of the current window
+    placement = "edge",
+
+    -- When the symbols change, resize the aerial window (within min/max constraints) to fit
+    resize_to_content = false,
+
+    -- Preserve window size equality with (:help CTRL-W_=)
+    preserve_equality = false,
   },
 
   float = {
@@ -40,4 +58,53 @@ require('aerial').setup({
   close_on_select = true,
   close_automatic_events = { 'unfocus' },
   attach_mode = 'window',
+
+  keymaps = {
+    ["?"] = "actions.show_help",
+    ["g?"] = "actions.show_help",
+    ["<CR>"] = "actions.jump",
+    ["<2-LeftMouse>"] = "actions.jump",
+    ["<C-v>"] = "actions.jump_vsplit",
+    ["<C-s>"] = "actions.jump_split",
+    ["p"] = "actions.scroll",
+    ["<C-j>"] = "actions.down_and_scroll",
+    ["<C-k>"] = "actions.up_and_scroll",
+    ["{"] = "actions.prev",
+    ["}"] = "actions.next",
+    ["[["] = "actions.prev_up",
+    ["]]"] = "actions.next_up",
+    ["q"] = "actions.close",
+
+    ["o"] = "actions.tree_toggle",
+    ["za"] = "actions.tree_toggle",
+    ["O"] = "actions.tree_toggle_recursive",
+    ["zA"] = "actions.tree_toggle_recursive",
+    ["l"] = "actions.tree_open",
+    ["zo"] = "actions.tree_open",
+    ["L"] = "actions.tree_open_recursive",
+    ["zO"] = "actions.tree_open_recursive",
+    ["h"] = "actions.tree_close",
+    ["zc"] = "actions.tree_close",
+    ["H"] = "actions.tree_close_recursive",
+    ["zC"] = "actions.tree_close_recursive",
+    ["zr"] = "actions.tree_increase_fold_level",
+    ["zR"] = "actions.tree_open_all",
+    ["zm"] = "actions.tree_decrease_fold_level",
+    ["zM"] = "actions.tree_close_all",
+    ["zx"] = "actions.tree_sync_folds",
+    ["zX"] = "actions.tree_sync_folds",
+  },
+
+  nav = {
+    keymaps = {
+      ["<CR>"] = "actions.jump",
+      ["<2-LeftMouse>"] = "actions.jump",
+      ["<C-v>"] = "actions.jump_vsplit",
+      ["<C-s>"] = "actions.jump_split",
+      ["p"] = "actions.scroll",
+      ["h"] = "actions.left",
+      ["l"] = "actions.right",
+      ["q"] = "actions.close",
+    },
+  }
 })
